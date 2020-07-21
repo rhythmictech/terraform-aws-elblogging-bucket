a4caa1a195ea66720674ea7417af4ec764acca79
Cris Daniluk <cdaniluk@rhythmictech.com>
2020-07-21 12:40:20 -0400

    major refactor - configurable lifecycle policies (#2)
    * major refactor - configurable lifecycle policies
    
    * Update versions.tf
    
    Co-authored-by: Steven B <genseb7@gmail.com>
    
    Co-authored-by: Steven B <genseb7@gmail.com>

--- variables.tf
+++ variables.tf
@@ -1,12 +1,20 @@
 
+variable "bucket_name" {
+  default     = null
+  description = "Name to apply to bucket (use `bucket_name` or `bucket_suffix`)"
+  type        = string
+}
 
 variable "bucket_suffix" {
   default     = "elblogging"
-  description = "Suffix to apply to bucket name"
+  description = "Suffix to apply to the bucket (use `bucket_name` or `bucket_suffix`). When using `bucket_suffix`, the bucket name will be `[ACCOUNT_ID]-[REGION]-s3logging-[BUCKET_SUFFIX]."
   type        = string
 }
 
 variable "elb_logging_regions" {
+  description = "Map of regions and account IDs to allow ELB logging access to"
+  type        = map(string)
+
   default = {
     us-east-1      = "127311923021"
     us-east-2      = "033677994240"
@@ -28,18 +36,42 @@ variable "elb_logging_regions" {
     me-south-1     = "076674570225"
     sa-east-1      = "507241528517"
   }
-  description = "Map of regions and account IDs to allow ELB logging access to"
-  type        = map(string)
-
 }
 
-variable "region" {
-  description = "Region to create logging bucket in"
+variable "lifecycle_rules" {
+  default     = []
+  description = "lifecycle rules to apply to the bucket"
+
+  type = list(object(
+    {
+      id                            = string
+      enabled                       = bool
+      prefix                        = string
+      expiration                    = number
+      noncurrent_version_expiration = number
+  }))
+}
+
+variable "s3_access_logging_bucket" {
+  default     = null
+  description = "Optional target for S3 access logging"
+  type        = string
+}
+
+variable "s3_access_logging_prefix" {
+  default     = null
+  description = "Optional target prefix for S3 access logging (only used if `s3_access_logging_bucket` is set)"
   type        = string
 }
 
 variable "tags" {
   default     = {}
-  description = "Mapping of any extra tags you want added to resources"
+  description = "Tags to add to supported resources"
   type        = map(string)
+}
+
+variable "versioning_enabled" {
+  default     = true
+  description = "Whether or not to use versioning on the bucket. This can be useful for audit purposes since objects in a logging bucket should not be updated."
+  type        = bool
 }
